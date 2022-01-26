8f18c8a8ede05190a8681b200c2f1190da1778b6
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-26 18:25:26 +0000

    displaying cookie omen and adding new

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-ReactDOM.render(<React.StrictMode><TicketShop /></React.StrictMode>, document.getElementById("root"))
+class Omen extends React.Component{
+state = {
+    omen: null,
+    newOmen: "",
+}
+
+omens = [
+"You will be rich!!!", 
+"You will be Frontend developer!!!",
+"You are going to travel far!!!"];
+    
+
+
+handleClick = () => {
+    const random = Math.floor(Math.random() * this.omens.length); 
+  this.setState({
+      omen: this.omens[random],
+  })
+}
+handleInputChange = (e) => {
+    this.setState({
+        newOmen: e.target.value,
+    })
+}
+handleAddOmen = () => {
+    this.omens.push(this.state.newOmen);
+    this.setState({
+        newOmen: "",
+    })
+}
+
+render(){
+    return(
+        <>
+        <button onClick={this.handleClick}>Show omen</button>
+        <br />
+            <input type="submit" onChange={this.handleInputChange} value={this.state.newOmen} type="text" />
+            <button onClick={this.handleAddOmen} >Add omen</button> 
+        {this.state.omen !== null ? <h1>{this.state.omen}</h1> : null}
+        </>
+    )
+}
+
+}
+
+
+
+ReactDOM.render(<React.StrictMode><Omen /></React.StrictMode>, document.getElementById("root"))
